de64326afcfed8545393faefeacca8f766fe3a62
Joey Reyes <joeyr1984@gmail.com>
2020-08-26 21:35:34 -0700

    added starter code for nav and styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -18,7 +18,29 @@ function Navbar() {
             <Link className="navbar-brand" to="/contact" >
                 Contact
             </Link>
+            <nav class="nav-area">
+        <div>
+            <img src="./images/logo-black.svg" class="nav-logo mt-4" alt="logo for HandCrafted Heirloom LLC ">
+        </div>
+        <ul class="nav flex-column mt-5
+        ">
+            <li class="nav-item">
+                <a class="nav-link navLinks mb-5" href="#">GALLERY</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link navLinks mb-5" href="#">SERVICES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link navLinks mb-5" href="#">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link navLinks mb-5" href="#">CONTACT</a>
+            </li>
+        </ul>
 
+
+
+    </nav>
         </nav>
     );
 }
