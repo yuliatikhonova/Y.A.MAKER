c989b72d52f05dae7116fc9af92c79e22c5dfecf
Michael Farris <farris.michael.j@gmail.com>
2020-09-02 19:23:31 -0700

    added modal for new items

--- client/src/components/Gallery/gallery.js
+++ client/src/components/Gallery/gallery.js
@@ -4,33 +4,35 @@ import "./style.css";
 function Gallery(props) {
     return (
         <section className="container gallery">
-            {/* !--Modal-- */}
-            {/* !-- Button trigger modal -- */}
             <button type="button" className="gallery-btn btn-dark" data-toggle="modal" data-target="#exampleModal">
                 Upload New Product
           </button>
-
-            {/* !-- Modal -- */}
-            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Upload New Product Thumbnail</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+            <form className="modal-dialog" id="newPostForm" action="/api/update" method="POST" enctype="multipart/form-data">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{{ this.city }}</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <input type="hidden" name="id" value="{{this.id}}"></input>
+                        <div className="form-group">
+                            <label for="imageUpload">Add Image</label>
+                            <input type="file" className="form-control-file" name="imageUpload"></input>
                         </div>
-                        <div className="modal-body">
-                            ...
-                </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-light">Load Product</button>
+                        <div className="form-group">
+                            <label for="countryName">Country Name</label>
+                            <input type="text" className="form-control" id="countryName" name="countryName" placeholder="Country"></input>
                         </div>
+                        <textarea type="text" className="blogPost" id="blogPost" name="blogPost"></textarea>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal" id="close-btn">Cancel</button>
+                        <button type="submit" className="btn btn-primary" id="submit-btn">Add</button>
                     </div>
                 </div>
-            </div>
-            {/* <!--Gallery--> */}
+            </form>
             <div className="col-12 mt-5">
 
                 <img src="/images/table.jpg" alt="table" className="img-fluid" />
