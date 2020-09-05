eec5778fd097b47fbd8c5975194d066216763db8
JerryForsberg <Jerryforsberg8991@gmail.com>
2020-09-04 20:28:51 -0700

    creating table for transactions and styling paypal button

--- client/src/components/Paypal/Paypal.js
+++ client/src/components/Paypal/Paypal.js
@@ -39,7 +39,14 @@ export default class Paypal extends React.Component {
 
         // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
         return (
-            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
+            <PaypalExpressBtn style={{
+
+                size: 'small',
+                color: 'black',
+                shape: 'rect',
+                label: 'checkout',
+                tagline: 'true'
+            }} env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
         );
     }
 }
