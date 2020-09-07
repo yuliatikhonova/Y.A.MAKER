3bdaddfa69a1bf61e6bd73bd31f6228f4002edfa
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-07 14:22:02 -0700

    starting to get gallery data

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -3,22 +3,23 @@ import Gallery from "../components/Gallery";
 
 function GalleryPage(props) {
     return (
-      < Gallery {...props.children} />
-                  {/* Map through data from database to display on the gallery page */}
-            {/* {data.map(gallery =>(
+        <div>
+            < Gallery {...props.children} />
+            {/* Map through data from database to display on the gallery page */}
+
+            {items.map(item => (
                 <Gallery
-                id = {gallery.id}
-                key = {gallery.key}
-                itemName= {.itemName}
-                itemPrice= {.itemPrice}
-                itemDescription= {.itemDescription}
-                imageUpload= {.imageUpload}
-
+                    id={item.id}
+                    key={item.key}
+                    itemName={item.itemName}
+                    itemPrice={item.itemPrice}
+                    itemDescription={item.itemDescription}
+                    imageUpload={item.imageUpload}
                 />
-            ))} */}
+            ))}
+        </div>
+    );
+}
 
-      );
-  }
-  
-  export default GalleryPage;
+export default GalleryPage;
 
