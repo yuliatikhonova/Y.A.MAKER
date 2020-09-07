1246d2b38833e369047c21cc814dfb8f469dd415
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-06 17:34:12 -0700

    added seeds for gallery items

--- scripts/products.sql
+++ scripts/products.sql
@@ -8,12 +8,13 @@ CREATE TABLE items(
   description VARCHAR,
 );
 
-INSERT INTO items (name, price, description) values("Play Tower with slide","$200", "Play tower for kids to climb on with slide attachment.");
-INSERT INTO items (name, price, description) values("Side Shelf", "$250", "Small single shelf.");
-INSERT INTO items (name, price, description) values("Standing Crib", "$300", "Small standing crib with lower shelf.");
-INSERT INTO items (name, price, description) values("Coffee Table", "$350", "Soft light colored wooden coffee table.");
-INSERT INTO items (name, price, description) values("Climbing Tower", "$250", "Colorfull climbing tower with rock wall attachment.");
-INSERT INTO items (name, price, description) values("Play Tower", "$175", "Play Tower for kids to climb on.");
-INSERT INTO items (name, price, description) values();
-INSERT INTO items (name, price, description) values();
-INSERT INTO items (name, price, description) values();
+INSERT INTO items (name, price, description) values("Tray"," $35", "The Wooden tray is a light tan color. It can be used as a side table or to eat by the tv.");
+INSERT INTO items (name, price, description) values("Stool", "$40", "This wooden stool has a unique cut out making it light weight.");
+INSERT INTO items (name, price, description) values("Plate Set", "$120", "This wooden plate set comes with 5 salad plates and 6 dinner plates.");
+INSERT INTO items (name, price, description) values("Wood Mortar and Pestle", "$25", "This small wooden mortar and pestle are great for grinding herbs.");
+INSERT INTO items (name, price, description) values("Lamp Shade", "$35", "This unique wooden lamp shade is compatible with a hanging light fixture or a lamp.");
+INSERT INTO items (name, price, description) values("Bean Bag Chair", "$150", "The bean bag chair offers a stylish seat for any home.");
+INSERT INTO items (name, price, description) values("Chair Set", "$375", "The chair set comes with 1 master chair and 3 stools.");
+INSERT INTO items (name, price, description) values("Ottoman", "$125", "Use this wooden ottoman inside or outside to add extra seating or as a table.");
+INSERT INTO items (name, price, description) values("Lamp Shade Set", "$65", "The lamp shade set comes with 1 wide basket covering and 1 small basket. Display them together or separately, for hanging lamps only.");
+INSERT INTO items (name, price, description) values("Armrest Coaster", "$20", "The armrest coaster offers a convient alternative to a side table.");
