534a2117b6b970c933189039d07f491b89da7699
Michael Farris <farris.michael.j@gmail.com>
2020-09-07 22:07:15 -0700

    added list and list item components

--- client/src/components/GalleryModal/index.js
+++ client/src/components/GalleryModal/index.js
@@ -3,41 +3,7 @@ import React from "react";
 
 function GalleryModal() {
     return (
-            <div className="modal fade" id="newPostModal_" data-backdrop="static" data-keyboard="false" tabindex="-1"
-                role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <form className="modal-dialog" id="newPostForm" action="/api/post" method="POST" enctype="multipart/form-data">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Add New Item</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="form-group">
-                                <label for="imageUpload">Add Image</label>
-                                <input type="file" className="form-control-file" name="imageUpload"></input>
-                            </div>
-                            <div className="form-group">
-                                <label for="itemName">Item Name</label>
-                                <input type="text" className="form-control" id="itemName" name="itemName" placeholder="Item Name"></input>
-                            </div>
-                            <div className="form-group">
-                                <label for="itemPrice">Item Price</label>
-                                <input type="text" className="form-control" id="itemPrice" name="itemPrice" placeholder="Item Price"></input>
-                            </div>
-                            <div className="form-group">
-                                <label for="itemDescription">Item Description</label>
-                                <textarea type="text" className="form-control" id="itemDescription" name="itemDescription"></textarea>
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal" id="close-btn">Cancel</button>
-                            <button type="submit" className="btn btn-primary" id="submit-btn">Add</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            
     );
 }
 
--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,33 +1,125 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Galleryimgs from "../components/Gallery";
 import GalleryModal from "../GalleryModal/";
 import API from "../../utils/API";
-class GalleryPage extends Component {
-    state = {
-        items
+function GalleryPage() {
+    const [items, setItems] = useState([])
+    const [formObject, setFormObject] = useState({
+        title: "",
+        author: "",
+        synopsis: ""
+      })
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadItems()
+    }, [])
+    function loadItems() {
+        API.getItems()
+            .then(res =>
+                setItems(res.data)
+            )
+            .catch(err => console.log(err));
     };
-    render() {
-        return (
-            <div>
-                <button type="button" className="gallery-btn btn-dark" data-toggle="modal" data-target="#newPostModal_">
-                    <FontAwesomeIcon icon="edit" />
-                </button>
-                <GalleryModal />
-                <div className="card-deck">
-                    {this.state.items.map(item => (
-                        <Galleryimgs
-                            id={item.id}
-                            key={item.key}
-                            itemName={item.itemName}
-                            itemPrice={item.itemPrice}
-                            itemDescription={item.itemDescription}
-                            imageUpload={item.imageUpload}
-                        />
-                    ))}
-                </div>
-            </div>
-        );
+
+    // Deletes an imte from the database with a given id, then reloads books from the db
+    function deleteItem(id) {
+        API.deleteItem(id)
+            .then(res => loadItems())
+            .catch(err => console.log(err));
     }
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+    };
+
+    // When the form is submitted, use the API.saveBook method to save the book data
+    // Then reload books from the database
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.itemName && formObject.itemPrice ) {
+            API.saveItem({
+                itemName: formObject.itemName,
+                itemPrice: formObject.itemPrice,
+                imageUpload: formObject.imageUpload, 
+            })
+                .then(() => setFormObject({
+                    itemName: "",
+                    itemPrice: "",
+                    itemDescription: "",
+                    imageUpload: ""
+                }))
+                .then(() => loadItems())
+                .catch(err => console.log(err));
+        }
+    };
+    return (
+        <div>
+            <button type="button" className="gallery-btn btn-dark" data-toggle="modal" data-target="#newPostModal_">
+              
+            </button>
+            <div>
+            <div className="modal fade" id="newPostModal_" data-backdrop="static" data-keyboard="false" tabindex="-1"
+                role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <form className="modal-dialog" id="newPostForm" action="/api/post" method="POST" enctype="multipart/form-data">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Add New Item</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <label for="imageUpload">Add Image</label>
+                                <input type="file" className="form-control-file" name="imageUpload"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="itemName">Item Name</label>
+                                <input type="text" className="form-control" id="itemName" name="itemName" placeholder="Item Name"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="itemPrice">Item Price</label>
+                                <input type="text" className="form-control" id="itemPrice" name="itemPrice" placeholder="Item Price"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="itemDescription">Item Description</label>
+                                <textarea type="text" className="form-control" id="itemDescription" name="itemDescription"></textarea>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal" id="close-btn">Cancel</button>
+                            <button type="submit" className="btn btn-primary" id="submit-btn">Add</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            </div>
+            {items.length ? (
+              <List>
+                {items.map(item => (
+                  <ListItem key={item._id}>
+                    <Link to={"/items/" + item._id}>
+                      <strong>
+                      <Galleryimgs
+                        id={item.id}
+                        key={item.key}
+                        itemName={item.itemName}
+                        itemPrice={item.itemPrice}
+                        itemDescription={item.itemDescription}
+                        imageUpload={item.imageUpload}
+                    />
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteItem(item._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+        </div>
+    );
 }
 
 export default GalleryPage;
