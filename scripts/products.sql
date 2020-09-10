87815110816c7c371a73f637388b9bb546cf67d4
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-09 19:28:46 -0700

    added seed images

--- scripts/products.sql
+++ scripts/products.sql
@@ -2,31 +2,31 @@ USE handcraftedheirloom;
 
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Tray", 35, "The Wooden tray is a light tan color. It can be used as a side table or to eat by the tv.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Tray", 35, "The Wooden tray is a light tan color. It can be used as a side table or to eat by the tv.", "https://res.cloudinary.com/tuktuk/image/upload/v1599704014/b4cxuek31kqwrenkjrvu.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Stool", 40, "This wooden stool has a unique cut out making it light weight.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Stool", 40, "This wooden stool has a unique cut out making it lightweight.", "https://res.cloudinary.com/tuktuk/image/upload/v1599704113/o47nrtlh5pe6tmgc451f.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Plate Set", 120, "This wooden plate set comes with 5 salad plates and 6 dinner plates.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Plate Set", 120, "This wooden plate set comes with 5 salad plates and 6 dinner plates.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704283/vbalv6a6iayya6wrnftg.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Wood Mortar and Pestle", 25, "This small wooden mortar and pestle are great for grinding herbs.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Wood Mortar and Pestle", 25, "This small wooden mortar and pestle are great for grinding herbs.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704364/cumzpu7pkuohl9nrqxcq.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Lamp Shade", 35, "This unique wooden lamp shade is compatible with a hanging light fixture or a lamp.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Lamp Shade", 35, "This unique wooden lamp shade is compatible with a hanging light fixture or a lamp.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704446/uvw57ck83ljae84vjc9k.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Bean Bag Chair", 150, "The bean bag chair offers a stylish seat for any home.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Bean Bag Chair", 150, "The bean bag chair offers a stylish seat for any home.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704524/ipvsgysq6q2fs86v4o2s.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Chair Set", 375, "The chair set comes with 1 master chair and 3 stools.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Chair Set", 375, "The chair set comes with 1 master chair and 3 stools.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704634/on6u5tnte4paxafddh6d.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Ottoman", 125, "Use this wooden ottoman inside or outside to add extra seating or as a table.", "https://res.cloudinary.com/tuktuk/image/upload/v1596239971/ghxb1cajmsmf4lrn5iy3.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Ottoman", 125, "Use this wooden ottoman inside or outside to add extra seating or as a table.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704710/ciqdtyvfnktbfpfm2ude.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Lamp Shade Set", 65, "The lamp shade set comes with 1 wide basket covering and 1 small basket. Display them together or separately, for hanging lamps only.", "https://res.cloudinary.com/tuktuk/image/upload/v1596080055/no5l4hehhsdqkjc3qp9k.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Lamp Shade Set", 65, "The lamp shade set comes with 1 wide basket covering and 1 small basket. Display them together or separately, for hanging lamps only.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704774/rgrvdajfinp41cnavd6v.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
 INSERT INTO items
     (itemName, itemPrice, itemDescription, imageUpload, createdAt, updatedAt)
-values("Armrest Coaster", 20, "The armrest coaster offers a convient alternative to a side table.", "https://res.cloudinary.com/tuktuk/image/upload/v1596079973/zukm014plssrahjmo4y9.jpg", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
+values("Armrest Coaster", 20, "The armrest coaster offers a convenient alternative to a side table.", "http://res.cloudinary.com/tuktuk/image/upload/v1599704858/hqshkbongnrfcwirz1ig.png", "2020-09-06 19:40:30", "2020-09-06 19:40:30");
