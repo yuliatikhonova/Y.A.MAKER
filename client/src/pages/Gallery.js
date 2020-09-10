cdabaa9ac3efbb4b5344397388453c0e67005e4c
yulia tikhonova <yuliatikhonova@outlook.com>
2020-09-09 20:56:01 -0700

    fixed the loged in and able to hide the modal

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,14 +1,42 @@
 import React from "react";
 import ItemsList from "../components/Gallery";
 import GalleryModal from "../components/GalleryModal/";
-function GalleryPage() {
+import axios from 'axios'
+
+const styles = {
+    btn: {
+        visibility: "visible"
+    }
+}
+
+function GalleryPage(props) {
+
+    // logoutForm.on("submit", function (event) {
+    //     event.preventDefault();//stops from refreshing the page
+    //     logoutUser();
+    // });
+
+    // function logoutUser() {
+    //     axios.post("/api/logout")
+    //         .then(function () {
+    //             window.location.replace("/");
+    //         })
+    //         .catch(function (err) {
+    //             console.log(err);
+    //         });
+    // }
 
 
     return (
         <div>
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal
-            </button>
+            {props.isLoggedin ? (<React.Fragment>
+                <button style={styles.btn} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                </button>
+                <button style={styles.btn} type="submit" href className="sign-up btn btn-outline-dark btn-sm">Log out</button>
+                </React.Fragment>
+            ) : ""}
+
             <GalleryModal />
             <ItemsList />
         </div>
