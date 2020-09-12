d7b9885bc8ae4d078b5fd12924b865050a7d23df
Michael Farris <farris.michael.j@gmail.com>
2020-09-12 14:08:07 -0700

    fixed modal refresh

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemsList from "../components/Gallery";
 import GalleryModal from "../components/GalleryModal/";
 import axios from 'axios'
 
 
 function GalleryPage(props) {
-
-    function logingOut () {
+    const [modalOpen, setModalOpen] = useState(false)
+    function logingOut() {
         axios.get("/logout")
             .then((data) => {
                 props.setIsLoggedin(false)
@@ -19,15 +19,14 @@ function GalleryPage(props) {
     return (
         <div>
             {props.isLoggedin ? (<React.Fragment>
-                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                <button type="button" className="btn btn-primary" onClick={() => setModalOpen(true)}>
                     Launch demo modal
                 </button>
                 <button onClick={() => logingOut()} type="submit" className="sign-up btn btn-outline-dark btn-sm">Log out</button>
             </React.Fragment>
             ) : ""}
 
-            <GalleryModal />
-            <ItemsList />
+            <ItemsList modalOpen={modalOpen} setModalOpen={setModalOpen} />
         </div>
     )
 }
