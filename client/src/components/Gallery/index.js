7038240b3c0d570ce0a7fafa8dbf3d9b21291b42
Michael Farris <farris.michael.j@gmail.com>
2020-09-14 18:27:31 -0700

    code cleanup on gallery

--- client/src/components/Gallery/index.js
+++ client/src/components/Gallery/index.js
@@ -10,7 +10,6 @@ export default class ItemsList extends Component {
     super(props);
     this.retrieveItems = this.retrieveItems.bind(this);
     this.refreshList = this.refreshList.bind(this);
-    this.searchName = this.searchName.bind(this);
 
     this.state = {
       items: [],
@@ -24,21 +23,12 @@ export default class ItemsList extends Component {
     this.retrieveItems();
   }
 
-  onChangeSearchName(e) {
-    const searchName = e.target.value;
-
-    this.setState({
-      searchName: searchName
-    });
-  }
-
   retrieveItems() {
     ItemDataService.getAll()
       .then(response => {
         this.setState({
           items: response.data
         });
-        console.log(response.data);
       })
       .catch(e => {
         console.log(e);
@@ -49,45 +39,21 @@ export default class ItemsList extends Component {
     this.retrieveItems();
   }
 
-
-  searchName() {
-    ItemDataService.findByName(this.state.searchName)
-      .then(response => {
-        this.setState({
-          items: response.data
-        });
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
   render() {
-    const { searchName, items, currentItem, currentIndex } = this.state;
+    const { items } = this.state;
 
     return (
       <div className="container">
         <GalleryModal refreshList={this.refreshList} open={this.props.modalOpen} setModalOpen={this.props.setModalOpen} />
         <div className="row">
           <div className="col-md-10 mx-auto">
-            {/* <h4>Items List</h4> */}
-
-            <div className=" ">
               {items &&
-                items.map((item, index) => (
-
-
-                  <Link
-                    to={"/items/" + item.id}
-                  >
+                items.map((item) => (
+                  <Link to={"/items/" + item.id}>
                     <img className="goonie" src={item.imageUpload} alt={item.itemName}>
                     </img>
                   </Link>
-
-
                 ))}
-            </div>
           </div>
         </div>
       </div >
